3932a34a8198188fb7814bf3ba21e3c3cee882d0
MSaiSankar <105710987+MSaiSankar@users.noreply.github.com>
2026-02-02 05:44:48 +0000

    Update models to use ObjectIdField as primary key for MongoDB compatibility

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,7 @@
 from djongo import models
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     class Meta:
         db_table = 'teams'
@@ -8,8 +9,13 @@ class Team(models.Model):
         return self.name
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    age = models.IntegerField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
     class Meta:
         db_table = 'users'
@@ -17,6 +23,7 @@ class User(models.Model):
         return self.name
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     type = models.CharField(max_length=100)
     duration = models.IntegerField()
@@ -25,6 +32,7 @@ class Activity(models.Model):
         db_table = 'activities'
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.CharField(max_length=100)
@@ -32,6 +40,7 @@ class Workout(models.Model):
         db_table = 'workouts'
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboard')
     points = models.IntegerField()
     class Meta:
